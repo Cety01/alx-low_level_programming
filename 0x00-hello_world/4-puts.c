21f389a46b6722099d54611ce3e2e55915a825e8
Cety01 <aymanofficiel@gmail.com>
2023-06-15 14:32:52 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/*
+ * Main
+ *
+ * Return zero
+ */
 int main(void)
-	{
-	    puts("Programming is like building a multilingual puzzle");
-	    return 0;
-	}
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
