6ceeae19bf06a2b596082dd9756611fe6d64c218
Cety01 <aymanofficiel@gmail.com>
2023-06-21 05:08:07 -0700

    C

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,34 +4,33 @@
   * times_table - prints the 9 times table
   *
   */
-  void times_table(void)
-  {
-	  int i;
-	  int j;
-	  
-	  for(i = 0; i <= 9; i++)
-	  {
+ void times_table(void)
+ {
+	 int i;
+	 int j;
+
+	 for(i = 0; i <= 9; i++)
+	 {
 		for(j = 0; j <= 9; j++)
-		  {
-			  int product = i * j;
-			  if (j == 0)
-			  {
+		{
+			 int product = i * j;
+			 if (j == 0)
+			 {
 				_putchar('0');
-			  } else if (j <= 9)
-			  {
+			 } else if (j <= 9)
+			 {
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(product + '0');
-			  } else
-			  {
+			 } else
+			 {
 				_putchar(',');
 				_putchar(' ');
 				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
-			  }
-		  }
-		_putchar('\n');		  
-	  }
-  }
-
+			 }
+		}
+		_putchar('\n');
+	 }
+ }
