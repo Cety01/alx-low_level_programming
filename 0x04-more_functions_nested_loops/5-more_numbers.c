a60c0dbc9d79a19027b5ea47c4f059a0633daaf0
Cety01 <aymanofficiel@gmail.com>
2023-06-22 09:25:18 -0700

    C

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,14 +13,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j <= 9)
-			{
-				_putchar(j + '0');
-			} else
+			if (j > 9)
 			{
 				_putchar('0' + j / 10);
-				_putchar('0' + j % 10);
 			}
+			_putchar('0' + j % 10);
 		}
 		_putchar('\n');
 	}
